31062da972ff6bcda5278b9e877fbdc3c6475fb3
王栋 <d-wan14@mails.tsinghua.edu.cn>
2017-04-11 19:43:22 +0800

    delete no use input

--- common.php
+++ common.php
@@ -27,30 +27,12 @@
     }
 
     $PREDICTION_PARAM = array(
-        "symboling",
-        "normalized-losses",
         "make",
-        "fuel-type",
-        "aspiration",
-        "num-of-doors",
         "body-style",
-        "drive-wheels",
-        "engine-location",
         "wheel-base",
-        "length",
-        "width",
-        "height",
-        "curb-weight",
-        "engine-type",
-        "num-of-cylinders",
         "engine-size",
-        "fuel-system",
-        "bore",
-        "stroke",
-        "compression-ratio",
         "horsepower",
         "peak-rpm",
-        "city-mpg",
         "highway-mpg",
         "price");
 ?>
